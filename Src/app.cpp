79dba8b360b4e3cca16ad433907f7d9f33789f50
Emanuel Redondo <eredo001@ucr.edu>
2022-04-03 19:52:31 -0400

    Frame Buffers

--- Src/app.cpp
+++ Src/app.cpp
@@ -115,7 +115,7 @@ int main(void)
 	glEnable(GL_DEPTH_TEST);
 
 	//Enable face culling
-	glEnable(GL_CULL_FACE);
+	//glEnable(GL_CULL_FACE);
 
 	/*Set callback for cursor controls*/
 	glfwSetCursorPosCallback(window, mouse_callback);
@@ -184,6 +184,17 @@ int main(void)
 		 5.0f, -0.5f, -5.0f,  2.0f, 2.0f
 	};
 
+	float quadVertices[] = {
+		// positions // texCoords
+		-1.0f, 1.0f, 0.0f, 1.0f,
+		-1.0f, -1.0f, 0.0f, 0.0f,
+		1.0f, -1.0f, 1.0f, 0.0f,
+		-1.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, -1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 1.0f, 1.0f
+	};
+
+
 	/*Buffers*/
 	VertexArray VAO;
 	VertexBuffer BoxVBO(BoxVertices, sizeof(BoxVertices));
@@ -210,19 +221,78 @@ int main(void)
 	PlaneVBO.UnBind();
 	PVAO.UnBind();
 
+	unsigned int ScreenVAO;
+	unsigned int ScreenVBO;
+
+	glGenVertexArrays(1, &ScreenVAO);
+	glBindVertexArray(ScreenVAO);
+
+	glGenBuffers(1, &ScreenVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, ScreenVBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 	Shader CubeShader("Shaders/CubeVert.glsl", "Shaders/CubeFrag.glsl");
+	Shader ScreenShader("Shaders/FrameVert.glsl", "Shaders/FrameFrag.glsl");
 
 	Texture BoxTex;
 	BoxTex.Bind();
-	BoxTex.LoadTexture("Textures/marble.jpg");
+	BoxTex.LoadTexture("Textures/container.jpg");
 
 	Texture PlaneTex;
 	PlaneTex.Bind();
 	PlaneTex.LoadTexture("Textures/metal.png");
 
+	CubeShader.use();
 	CubeShader.setInt("texture1", 0);
+
 	//BoxTex.Bind();
-	
+
+	/*FrameBuffer and RenderBuffer*/
+	unsigned int FBO;
+	glGenFramebuffers(1, &FBO);
+	glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+	unsigned int FrameTex;
+	glGenTextures(1, &FrameTex);
+	glBindTexture(GL_TEXTURE_2D, FrameTex);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	//glBindTexture(GL_TEXTURE_2D, 0);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, FrameTex, 0);
+
+	unsigned int RBO;
+	glGenRenderbuffers(1, &RBO);
+	glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 800, 600);
+	//glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
+
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+	{
+		std::cout << "ERROR::FRAMEBUFFER:: framebuffer is not complete." << std::endl;
+	}
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	ScreenShader.use();
+	ScreenShader.setInt("screenTexture", 0);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
@@ -231,11 +301,15 @@ int main(void)
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
 		ProcessKeyInput(window, deltaTime);
 
+		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+		glEnable(GL_DEPTH_TEST);
 
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		CubeShader.use();
 		/*Box model matrix*/
 		glm::mat4 model(1.0f);
 		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
@@ -248,34 +322,41 @@ int main(void)
 		glm::mat4 projection(1.0f);
 		projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
 
-
 		CubeShader.setMat4f("model", model);
 		CubeShader.setMat4f("view", view);
 		CubeShader.setMat4f("projection", projection);
 
-		glEnable(GL_CULL_FACE);
 		VAO.Bind();
-		CubeShader.use();
 		BoxTex.Bind();
 		BoxTex.ActivateTexture(0);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
-		
+
 		//glm::mat4 model(1.0f);
 		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
 		CubeShader.setMat4f("model", model);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		VAO.UnBind();
-
 		PVAO.Bind();
-		CubeShader.use();
 		PlaneTex.Bind();
 		PlaneTex.ActivateTexture(0);
 		model = glm::mat4(1.0f);
 		CubeShader.setMat4f("model", model);
-		glDisable(GL_CULL_FACE);
+		//glDisable(GL_CULL_FACE);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 		PVAO.UnBind();
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glDisable(GL_DEPTH_TEST);
+
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ScreenShader.use();
+		glBindVertexArray(ScreenVAO);
+		glBindTexture(GL_TEXTURE_2D, FrameTex);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 
@@ -283,6 +364,8 @@ int main(void)
 		glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &ScreenVAO);
+	glDeleteBuffers(1, &ScreenVBO);
 	glfwTerminate();
 	return 0;
 }
