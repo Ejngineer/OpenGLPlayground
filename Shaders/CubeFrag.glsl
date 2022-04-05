dddafe9cb778bf2e7a9e25acc4b2de1bdc57b471
Emanuel Redondo <eredo001@ucr.edu>
2022-04-04 21:32:28 -0400

    basic refraction. Note: Try double refraction

--- Shaders/CubeFrag.glsl
+++ Shaders/CubeFrag.glsl
@@ -11,7 +11,8 @@ uniform samplerCube cubeMap;
 
 void main()
 {
+	float ratio = 1.0/1.52; 
 	vec3 I = normalize(Position - camPos);
-	vec3 R = reflect(I, normalize(Normal));
+	vec3 R = refract(I, normalize(Normal), ratio);
 	FragColor = vec4(texture(cubeMap, R).rgb, 1.0);
 }
